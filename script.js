c1398337e7e9d4e771ce36b8e552c56e4a571331
Akash Masane <masaneakash23@gmail.com>
2025-07-08 21:50:40 +0530

    First commit

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function toggleMenu() {
+// Toggle hamburger menu
+    function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  // Auto-update year in footer
+  document.getElementById("year").textContent = new Date().getFullYear();
